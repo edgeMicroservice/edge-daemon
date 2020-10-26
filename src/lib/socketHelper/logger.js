12b4cb101b57bf9b3ba244f37e3494af5229f3ea
Kevin Toor <kevin.toor@mimik.com>
2020-10-26 14:46:25 -0700

    EPOC-336 test

--- src/lib/socketHelper/logger.js
+++ src/lib/socketHelper/logger.js
@@ -1,9 +1,9 @@
 const { addLogsById } = require('../../models/daemonModel');
 
 const makeLogger = (nodeId) => {
-  const log = (text) => {
-    console.log('===> edge socket logger', { nodeId, text });
-    addLogsById(nodeId, text);
+  const log = (text, data) => {
+    console.log('===> edge socket logger', { nodeId, text, data });
+    addLogsById(nodeId, `${text}: ${data}`);
   };
 
   return {
