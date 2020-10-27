6c125c57f600550f31f4b21b40766e6550447862
Kevin Toor <kevin.toor@mimik.com>
2020-10-27 09:15:54 -0700

    EPOC-336 test

--- src/processors/nodeProcessor.js
+++ src/processors/nodeProcessor.js
@@ -3,14 +3,9 @@ const daemonModel = require('../models/daemonModel');
 
 const getNodes = (mdeployStatuses, correlationId) => nodeModel.getAllNodes(correlationId)
   .then((nodes) => {
-    console.log('===> mdeployStatuses', mdeployStatuses);
     if (!mdeployStatuses) return nodes;
 
-    let mdeployStatusArr;
-    if (!Array.isArray(mdeployStatuses)) mdeployStatusArr = [mdeployStatuses];
-    else mdeployStatusArr = mdeployStatuses;
-
-    return nodes.filter((node) => mdeployStatusArr.includes(node.mdeployStatus));
+    return nodes.filter((node) => mdeployStatuses.includes(node.mdeployStatus));
   });
 
 const getDaemon = (nodeId, correlationId) => daemonModel.getDaemonById(nodeId, correlationId);
